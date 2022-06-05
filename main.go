a75524e37cab5f4d3b1c9238f861c46a5c0f8552
Julian Beck <ju-fa-beck@t-online.de>
2022-06-05 12:28:54 +0200

    feat(mergeIntervals): Basic Implementation

--- main.go
+++ main.go
@@ -5,29 +5,41 @@ import (
 	"sort"
 )
 
+//Merges an array of intervals into a single interval.
 func MergeIntervals(intervals [][]int) ([][]int, error) {
 
-	//return empty array if input is empty
+	//return empty array if intervals is empty
 	if len(intervals) == 0 {
-		return intervals, nil
+		return [][]int{}, nil
 	}
 
-	//sort intervals by start time
+	// sort intervals by first value
 	sort.Slice(intervals, func(i, j int) bool {
 		return intervals[i][0] < intervals[j][0]
 	})
+	// start with first interval in result, because we are sorting by first value
+	// the first interval in the result will contain the smallest first value
 	results := [][]int{intervals[0]}
 
-	for i := 1; i < len(intervals)-1; i++ {
-		if intervals[i][0] <= results[i][1] {
-
-			results[i][1] = intervals[i][1]
+	for i := 1; i < len(intervals); i++ {
+		//check if the current interval overlaps with the last interval in the result
+		if intervals[i][0] <= results[len(results)-1][1] {
+			//if so, update the last interval in the result to include the end of the current interval
+			results[len(results)-1][1] = maxOf(results[len(results)-1][1], intervals[i][1])
 		} else {
+			//if not, add the current interval to the result
 			results = append(results, intervals[i])
 		}
 	}
 
 	return results, nil
+}
+
+func maxOf(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
 
 func main() {
