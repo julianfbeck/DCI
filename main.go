c59c0758c1b9926e32995e365a655fb248b987a3
Julian Beck <ju-fa-beck@t-online.de>
2022-06-05 10:44:30 +0200

    feat(mergeIntervals): Sort array

--- main.go
+++ main.go
@@ -1,12 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 func MergeIntervals(intervals [][]int) ([][]int, error) {
+
+	//return empty array if input is empty
 	if len(intervals) == 0 {
 		return intervals, nil
 	}
-	return intervals, nil
+
+	//sort intervals by start time
+	sort.Slice(intervals, func(i, j int) bool {
+		return intervals[i][0] < intervals[j][0]
+	})
+	results := [][]int{intervals[0]}
+
+	for i := 1; i < len(intervals)-1; i++ {
+		if intervals[i][0] <= results[i][1] {
+
+			results[i][1] = intervals[i][1]
+		} else {
+			results = append(results, intervals[i])
+		}
+	}
+
+	return results, nil
 }
 
 func main() {
